6650288685dd2ed3a5c35b9bc74a9abafe56d579
Marla Narazani <marlanarazani@gmail.com>
2019-10-15 02:09:06 +0200

    added short vibration for restart

--- Hardware/NiB.py
+++ Hardware/NiB.py
@@ -9,7 +9,8 @@ pad_pin = board.D23
 pad = DigitalInOut(pad_pin)
 pixels = neopixel.NeoPixel(board.D24,30)
 pad.direction = Direction.INPUT
-# Motor vibration via PWM
+# Motor vibration via PWM.
+# Mapping: r_1 => right glove, first finger is thumb l_1 => left hand, first finger is small finger
 r_1 = PWMOutputDevice(14)
 r_2 = PWMOutputDevice(15)
 r_3 = PWMOutputDevice(18)
@@ -30,16 +31,27 @@ motors = [r_1, r_2, r_3, r_4, r_5, l_1, l_2, l_3, l_4, l_5]
 while True:
         if pad.value and not device_on:
                 print("Turn on")
+                # Short double vibration to indicate turning on the device
                 for m in motors:
-                    m.value = 0.8
-                device_on = pad.value
+                        m.value = 1
+                time.sleep(0.3)
+                for m in motors:
+                        m.value = 0.0
+                time.sleep(0.3)
+                for m in motors:
+                        m.value = 1
+                time.sleep(0.3)
+                for m in motors:
+                        m.value = 0.0
+                device_on = True
                 pixels.fill((255,0,0))
                 pixels.show()
                 # Sleep to avoid conflicts
                 time.sleep(1)
         elif pad.value and device_on:
-                motor.value = 0.0
                 print("Turn off")
+                for m in motors:
+                        m.value = 0.0
                 device_on = False
                 pixels.fill((0,0,0))
                 pixels.show()
