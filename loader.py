472c4efaea60b41cbf474851efa08b7147329c11
nagam11 <marlanarazani@gmail.com>
2019-10-17 22:42:06 +0200

    Refactor tflite loader

--- loader.py
+++ loader.py
@@ -18,22 +18,3 @@ class Loader:
 
     def predict(self, data):
         return self.model.predict(data)
-
-
-class TFLiteLoader:
-    def __init__(self, path: str):
-        converter = tf.lite.TFLiteConverter.from_keras_model_file(path)
-        self.tflite_encoder = converter.convert()
-        # Load TFLite model and allocate tensors.
-        self.interpreter = tf.lite.Interpreter(model_content=self.tflite_encoder)
-        self.interpreter.allocate_tensors()
-
-        # Get input and output tensors.
-        self.input_details = self.interpreter.get_input_details()
-        self.output_details = self.interpreter.get_output_details()
-
-    def predict(self, data):
-        self.interpreter.set_tensor(self.input_details[0]['index'], data)
-        self.interpreter.invoke()
-        tflite_result = self.interpreter.get_tensor(self.output_details[0]['index'])
-        return tflite_result
